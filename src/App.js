fa46597c66d7f07139f39adca5674fb973e3a9b5
Theovady Moutty <theomoutty@gmail.com>
2020-12-11 10:41:47 +0100

    no message

--- src/App.js
+++ src/App.js
@@ -30,7 +30,6 @@ import Row from './Row';
 import Banner from './Banner';
 import Nav from './Nav';
 import { Route, Switch } from "react-router-dom";
-import { Dashboard } from "./components/Dashboard/Dashboard.js";
 import { Login } from "./components/Login/Login.js";
 import { Signup } from "./components/Signup/Signup.js";
 import { PrivateRoute } from "./components/PrivateRoute.js";
@@ -46,10 +45,7 @@ class App extends Component {
         fetchUrl={requests}
         isLargeRow={true}
         />
-        <Switch>
-          <Route exact path="/" component={Login} />
-          <Route exact path="/signup" component={Signup} />
-        </Switch>
+    
       {/*<Row title = "Trending Now" fetchUrl={requests.fetchTrending}
        isLargeRow={true}/>
       
